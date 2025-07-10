9e280b24934da588decec47d0d4dfbe9e5feccb8
gabmin <rlarkqals@gmail.com>
2025-07-10 13:32:04 +0900

    파이썬 3차시 list

---
+++ python/ch04/list1.py
@@ -0,0 +1,16 @@
+candies =  ['딸기맛', '레몬맛', '수박맛', '박하맛', '우유맛']
+print(candies)
+
+print(candies[0])
+print(candies[1])
+print(candies[2])
+print(candies[3])
+print(candies[4])
+
+lista = [1, 'python', '3.9', 3.14, '프로그래밍', [1, 'hi'], False]
+print(lista)
+
+
+candies[4] = '사과맛'
+candies.append('메론맛')
+candies.append('포도맛')
