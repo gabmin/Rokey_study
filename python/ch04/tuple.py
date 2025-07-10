47adcf2936898584fe796ab4f1e6b405ed2239fe
gabmin <rlarkqals@gmail.com>
2025-07-10 15:55:31 +0900

    파이썬 3차시 tuple

---
+++ python/ch04/tuple.py
@@ -0,0 +1,17 @@
+clovers = ('클로버1', '클로버2', '클로버3')
+
+print(type(clovers))
+print(clovers[1])
+
+my_tuple = 1, -2, 3.14, True, 'hi', [1, 'tt']
+
+print(my_tuple)
+
+my_int = (1)
+print(type(my_int))
+
+my_tuple2 = (1,)
+print(type(my_tuple2))
+
+my_list = list(my_tuple)
+print(type(my_list))
