02b32c8ce3d95d4b355e519687898a67cb327a35
gabmin <rlarkqals@gmail.com>
2025-07-11 10:10:30 +0900

    파이썬 4차시 과제

--- python/ch04/task.py
+++ python/ch04/task.py
@@ -10,11 +10,9 @@
 
 
 # nums = [1, 2, 3, 4, 5]
-# result = 0
-# for num in nums:
-#     result += num
 #
-# print(result)
+# print(sum(nums))
+
 
 # cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "쏘세지", "라면", "팥빙수", "김치전"]
 #
@@ -30,14 +28,14 @@
 # t = ('A', 'b', 'c')
 # print(t)
 
-ice_cream = {
-    '메로나': 1000,
-    '폴라포': 1200,
-    '빵빠레': 1800
-}
-
-ice_cream['죠스바'] = 1200
-ice_cream['월드콘'] = 1500
-ice_cream['메로나'] = 1300
-
-print(ice_cream)
+# ice_cream = {
+#     '메로나': 1000,
+#     '폴라포': 1200,
+#     '빵빠레': 1800
+# }
+#
+# ice_cream['죠스바'] = 1200
+# ice_cream['월드콘'] = 1500
+# ice_cream['메로나'] = 1300
+#
+# print(ice_cream)
