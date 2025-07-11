439727e9140390635b946e739b938d7df7c1f298
gabmin <rlarkqals@gmail.com>
2025-07-11 12:07:59 +0900

    파이썬 5차시 range

---
+++ python/ch05/range.py
@@ -0,0 +1,11 @@
+print(range(10))
+print(list(range(10)))
+print(list(range(1, 3)))
+
+for i in range(1, 3):
+    print(i)
+
+print(list(range(10, 1, -1)))
+
+for i in range(10, 1, -1):
+    print(i)
