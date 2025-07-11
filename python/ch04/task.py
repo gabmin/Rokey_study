75e9b3aeaeac3233174e28b81c07dbc5c6dd7340
gabmin <rlarkqals@gmail.com>
2025-07-11 10:06:58 +0900

    파이썬 4차시 과제

---
+++ python/ch04/task.py
@@ -0,0 +1,43 @@
+# movie_rank = ['하얼빈', '무파사:라이온킹', '소방관']
+#
+# movie_rank.append('위키드')
+#
+# movie_rank.insert(2, '모아나2')
+#
+# movie_rank.remove('소방관')
+#
+# print(movie_rank)
+
+
+# nums = [1, 2, 3, 4, 5]
+# result = 0
+# for num in nums:
+#     result += num
+#
+# print(result)
+
+# cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "쏘세지", "라면", "팥빙수", "김치전"]
+#
+# print(len(cook))
+
+# t = 1, 2, 3, 4
+#
+# print(type(t))
+
+
+# t = ('a', 'b', 'c')
+# print(t)
+# t = ('A', 'b', 'c')
+# print(t)
+
+ice_cream = {
+    '메로나': 1000,
+    '폴라포': 1200,
+    '빵빠레': 1800
+}
+
+ice_cream['죠스바'] = 1200
+ice_cream['월드콘'] = 1500
+ice_cream['메로나'] = 1300
+
+print(ice_cream)
