b19acfe75552378c0198c855b5861d56a43b1e09
gabmin <rlarkqals@gmail.com>
2025-07-24 15:59:49 +0900

    파이썬 13주차 정규표현식

---
+++ python/ch14/re1.py
@@ -0,0 +1,50 @@
+import re
+
+p = re.compile("[ab]")
+print(p.match("a"))
+print(p.match("before"))
+print(p.match("dude"))
+
+p = re.compile("[0-9]")
+print(p.match("5"))
+
+p = re.compile("[^0-9]")
+print(p.match("5"))
+
+# p = re.compile("[\^0-9]")
+# print(p.match("^"))
+#
+# p = re.compile("\d")
+# print(p.match("5"))
+#
+# p = re.compile("\s")
+# print(p.match("\n"))
+
+p = re.compile("a.b")
+print(p.match("abb"))
+
+p = re.compile("ca*t")
+print(p.match("caaaaat"))
+
+p = re.compile("ca+t")
+print(p.match("ct"))
+
+p = re.compile("ca{2}t")
+print(p.match("caat"))
+
+print("---------------")
+
+p = re.compile("^hello")
+print(p.match("hello world"))
+print(p.match(" hello world"))
+print(p.match("\nhello world"))
+print(p.match("dello world"))
+
+print("---------------")
+
+p = re.compile("world$")
+print(p.match("hello world"))
+print(p.search("hello world"))
+print(p.match("hello world!"))
+print(p.match("hello world\n"))
+print(p.match("hello world$"))
