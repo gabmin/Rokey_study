bbfb6469ed147fd2de98cfa4abee005d0db8e0eb
gabmin <rlarkqals@gmail.com>
2025-07-31 16:24:26 +0900

    파이썬 19주차 matplotlib modules

---
+++ python/ch19/matplotlib_module.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+# x = [1, 2, 3, 4]
+# y = [10, 20, 25, 30]
+
+# plt.plot(x, y)
+# plt.title("Line plot")
+# plt.xlabel("time")
+# plt.ylabel("value")
+# plt.show()
+
+categories = ["A", "B", "C", "D"]
+values = [3, 7, 8, 5]
+plt.bar(categories, values)
+plt.show()
